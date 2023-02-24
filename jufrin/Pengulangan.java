6f9bf5fe09bba5a0c6487123c937694287213c74
jufrin01 <125728570+jufrin01@users.noreply.github.com>
2023-02-24 23:13:55 +0700

    jufrin

---
+++ jufrin/Pengulangan.java
@@ -0,0 +1,13 @@
+package jufrin;
+
+public class Pengulangan {
+    public static void main(String[] args) {
+
+        int a,b ;
+        for (a=0; a>=10; a++){
+
+        }
+        System.out.println( );
+
+    }
+}
