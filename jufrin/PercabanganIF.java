99de57d9d13fb44ac6da4b4e8c340d5a5f096a46
jufrin01 <125728570+jufrin01@users.noreply.github.com>
2023-02-25 01:13:32 +0700

    jufrin

---
+++ jufrin/PercabanganIF.java
@@ -0,0 +1,21 @@
+package jufrin;
+
+public class PercabanganIF {
+    public static void main(String[] args) {
+        int kacangku =10;
+        if (kacangku < 5){
+
+            System.out.println("itu kacang ku");
+        }
+        else if (kacangku >20){
+            System.out.println("itu bukan kacang ku");
+        }
+        else {
+            System.out.println("GAK SUKA KACANG BRO");
+        }
+
+
+    }
+
+
+}
---
+++ jufrin/PercabanganIf0.java
@@ -0,0 +1,21 @@
+package jufrin;
+
+public class PercabanganIf0 {
+
+    public static void main(String[] args) {
+        int kacangLari = 1;
+
+        while (kacangLari < 20 ){
+            System.out.print("LARI......"+kacangLari);
+
+            if (kacangLari == 20) {
+                break;
+        }
+            kacangLari++;
+            System.out.println("KEJARR......");
+        }
+
+
+
+    }
+}
