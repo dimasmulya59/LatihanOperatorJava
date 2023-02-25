8f8fe457ddd3d52d38aa0fe7a8c9a1599fe732f8
indahseptierina <iseptierina@gmail.com>
2023-02-25 21:51:21 +0700

    oke

---
+++ indah/ContohScanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ContohScanner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan nama anda ");
+        String nama = input.nextLine();
+        System.out.println("Nama Anda= " + nama);
+    }
+    
+}
---
+++ indah/ContohScanner2.java
@@ -0,0 +1,9 @@
+public class ContohScanner2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan nama anda ");
+        int nomerhp = input,nextint();
+        System.out.println("Nomer Hp Anda = ");
+    }
+    
+}
